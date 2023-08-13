fb6b5fb269f1d5766a707e827db4f0bca6144604
dwesterwick <dwesterwick@yahoo.com>
2023-08-13 15:39:28 -0400

    updated server code

---
+++ src/CommonUtils.ts
@@ -0,0 +1,49 @@
+import modConfig from "../config/config.json";
+
+import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
+import { IDatabaseTables } from "@spt-aki/models/spt/server/IDatabaseTables";
+import { LocaleService } from "@spt-aki/services/LocaleService";
+
+export class CommonUtils
+{
+    private debugMessagePrefix = "[Questing Bots] ";
+    private translations: Record<string, string>;
+	
+    constructor (private logger: ILogger, private databaseTables: IDatabaseTables, private localeService: LocaleService)
+    {
+        // Get all translations for the current locale
+        this.translations = this.localeService.getLocaleDb();
+    }
+	
+    public logInfo(message: string, alwaysShow = false): void
+    {
+        if (modConfig.enabled || alwaysShow)
+            this.logger.info(this.debugMessagePrefix + message);
+    }
+
+    public logWarning(message: string): void
+    {
+        this.logger.warning(this.debugMessagePrefix + message);
+    }
+
+    public logError(message: string): void
+    {
+        this.logger.error(this.debugMessagePrefix + message);
+    }
+
+    public getItemName(itemID: string): string
+    {
+        const translationKey = itemID + " Name";
+        if (translationKey in this.translations)
+            return this.translations[translationKey];
+		
+        // If a key can't be found in the translations dictionary, fall back to the template data if possible
+        if (!(itemID in this.databaseTables.templates.items))
+        {
+            return undefined;
+        }
+
+        const item = this.databaseTables.templates.items[itemID];
+        return item._name;
+    }
+}
--- src/mod.ts
+++ src/mod.ts
@@ -1,23 +1,36 @@
 import modConfig from "../config/config.json";
+import { CommonUtils } from "./CommonUtils";
 
 import { DependencyContainer } from "tsyringe";
-import { IPreAkiLoadMod } from "@spt-aki/models/external/IPreAkiLoadMod";
-import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
-import { IPostAkiLoadMod } from "@spt-aki/models/external/IPostAkiLoadMod";
+import type { IPreAkiLoadMod } from "@spt-aki/models/external/IPreAkiLoadMod";
+import type { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
+import type { IPostAkiLoadMod } from "@spt-aki/models/external/IPostAkiLoadMod";
+import type { StaticRouterModService } from "@spt-aki/services/mod/staticRouter/StaticRouterModService";
+import type { DynamicRouterModService } from "@spt-aki/services/mod/dynamicRouter/DynamicRouterModService";
 
 import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
-import { StaticRouterModService } from "@spt-aki/services/mod/staticRouter/StaticRouterModService";
+import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
+import { IDatabaseTables } from "@spt-aki/models/spt/server/IDatabaseTables";
+import { LocaleService } from "@spt-aki/services/LocaleService";
+import { QuestHelper } from "@spt-aki/helpers/QuestHelper";
 
 const modName = "SPTQuestingBots";
 
 class QuestingBots implements IPreAkiLoadMod, IPostAkiLoadMod, IPostDBLoadMod
 {
+    private commonUtils: CommonUtils
+
     private logger: ILogger;
+    private databaseServer: DatabaseServer;
+    private databaseTables: IDatabaseTables;
+    private localeService: LocaleService;
+    private questHelper: QuestHelper;
 	
     public preAkiLoad(container: DependencyContainer): void 
     {
         this.logger = container.resolve<ILogger>("WinstonLogger");
         const staticRouterModService = container.resolve<StaticRouterModService>("StaticRouterModService");
+        const dynamicRouterModService = container.resolve<DynamicRouterModService>("DynamicRouterModService");
 		
         // Get config.json settings for the bepinex plugin
         staticRouterModService.registerStaticRouter(`StaticGetConfig${modName}`,
@@ -29,12 +42,72 @@ class QuestingBots implements IPreAkiLoadMod, IPostAkiLoadMod, IPostDBLoadMod
                 }
             }], "GetConfig"
         ); 
+        
+        // Get the logging directory for bepinex crash reports
+        staticRouterModService.registerStaticRouter(`StaticGetLoggingPath${modName}`,
+            [{
+                url: "/SPTQuestingBots/GetLoggingPath",
+                action: () => 
+                {
+                    return JSON.stringify({ path: __dirname + "/../log/" });
+                }
+            }], "GetLoggingPath"
+        );
 
+        // Report error messages to the SPT-AKI server console in case the user hasn't enabled the bepinex console
+        dynamicRouterModService.registerDynamicRouter(`DynamicReportError${modName}`,
+            [{
+                url: "/SPTQuestingBots/ReportError/",
+                action: (url: string) => 
+                {
+                    const urlParts = url.split("/");
+                    const errorMessage = urlParts[urlParts.length - 1];
+
+                    const regex = /%20/g;
+                    this.commonUtils.logError(errorMessage.replace(regex, " "));
+
+                    return JSON.stringify({ resp: "OK" });
+                }
+            }], "ReportError"
+        );
+
+        if (!modConfig.enabled)
+        {
+            return;
+        }
+
+        // Set PMC conversion to 100%
+        staticRouterModService.registerStaticRouter(`StaticForcePMCSpawns${modName}`,
+            [{
+                url: "/SPTQuestingBots/ForcePMCSpawns",
+                action: () => 
+                {
+                    
+                    return JSON.stringify({ resp: "OK" });
+                }
+            }], "ForcePMCSpawns"
+        );
+
+        // Get all quest templates
+        staticRouterModService.registerStaticRouter(`GetAllQuestTemplates${modName}`,
+            [{
+                url: "/SPTQuestingBots/GetAllQuestTemplates",
+                action: () => 
+                {
+                    return JSON.stringify({ quests: this.questHelper.getQuestsFromDb() });
+                }
+            }], "GetAllQuestTemplates"
+        );
     }
 	
-    public postDBLoad(): void
+    public postDBLoad(container: DependencyContainer): void
     {
-        
+        this.databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+        this.localeService = container.resolve<LocaleService>("LocaleService");
+        this.questHelper = container.resolve<QuestHelper>("QuestHelper");
+
+        this.databaseTables = this.databaseServer.getTables();
+        this.commonUtils = new CommonUtils(this.logger, this.databaseTables, this.localeService);
     }
 	
     public postAkiLoad(): void
