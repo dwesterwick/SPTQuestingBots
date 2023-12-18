3c029f9574cc875877b71c5e18432ffa41255054
dwesterwick <dwesterwick@yahoo.com>
2023-12-17 23:17:15 -0500

    Improved handling of missing quest files

--- src/QuestManager.ts
+++ src/QuestManager.ts
@@ -47,6 +47,8 @@ export class QuestManager
     public validateCustomQuests() : void
     {
         const path = __dirname + "/../quests";
+        let totalStandardQuestsAdded = 0;
+        let totalCustomQuestsAdded = 0;
 
         // Ensure the directory for standard quests exists
         if (this.vfs.exists(path + "/standard/"))
@@ -58,13 +60,19 @@ export class QuestManager
 
                 // If the JSON file can be parsed into a Quest array, assume it's fine
                 const quests : Quest[] = JSON.parse(questFileText);
+                totalStandardQuestsAdded += quests.length;
 
                 this.commonUtils.logInfo(`Found ${quests.length} standard quest(s) in "/standard/${standardQuests[i]}"`);
+            }
+
+            if (totalStandardQuestsAdded == 0)
+            {
+                this.commonUtils.logError("No standard quests have been added. Mod files may have been corrupted. Please try reinstalling.");
             }
         }
         else
         {
-            this.commonUtils.logError("The \"/quests/standard/\" directory is missing.");
+            this.commonUtils.logError("The \"user\\mods\\DanW-SPTQuestingBots\\quests\\standard\" directory is missing. Mod files may have been corrupted. Please try reinstalling.");
         }
 
         // Check if the directory for custom quests exists
@@ -77,13 +85,15 @@ export class QuestManager
 
                 // If the JSON file can be parsed into a Quest array, assume it's fine
                 const quests : Quest[] = JSON.parse(questFileText);
+                totalCustomQuestsAdded += quests.length;
 
                 this.commonUtils.logInfo(`Found ${quests.length} custom quest(s) in "/custom/${customQuests[i]}"`);
             }
-        }
-        else
-        {
-            this.commonUtils.logWarning("No custom quests found.");
+
+            if (totalCustomQuestsAdded == 0)
+            {
+                this.commonUtils.logWarning("No custom quests found.");
+            }
         }
     }
 }
