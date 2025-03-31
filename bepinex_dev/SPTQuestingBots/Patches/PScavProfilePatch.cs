29393c0e23c84c1e6d4db21561a9e31fdaa1afcc
ozen-m <74519617+ozen-m@users.noreply.github.com>
2025-03-31 21:30:52 +0800

    Use WillBeAPlayerScav(), Cleanup

--- bepinex_dev/SPTQuestingBots/Patches/PScavProfilePatch.cs
+++ bepinex_dev/SPTQuestingBots/Patches/PScavProfilePatch.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using EFT;
 using SPT.Reflection.Patching;
-using SPTQuestingBots.Controllers;
+using SPTQuestingBots.Helpers;
 
 namespace SPTQuestingBots.Patches
 {
@@ -28,7 +24,7 @@ namespace SPTQuestingBots.Patches
                 {
                     // Filter list to only pscavs, if none - the client requests from the server
                     // Use Info.Settings.Role instead of Side. Server always return x.Side == EPlayerSide.Savage
-                    List<Profile> filteredProfiles = __instance.list_0.ApplyFilter(x => x.Info.Settings.Role == WildSpawnType.assault && x.Nickname.Contains("("));
+                    List<Profile> filteredProfiles = __instance.list_0.ApplyFilter(x => x.Info.Settings.Role == WildSpawnType.assault && x.WillBeAPlayerScav());
                     __result = data.ChooseProfile(filteredProfiles, withDelete);
 
                     return false;
@@ -36,7 +32,7 @@ namespace SPTQuestingBots.Patches
                 else
                 {
                     // Filter list to only scavs (No pmc nickname)
-                    List<Profile> filteredProfiles = __instance.list_0.ApplyFilter(x => (x.Info.Settings.Role == WildSpawnType.assault && !x.Nickname.Contains("(") || x.Info.Settings.Role != WildSpawnType.assault));
+                    List<Profile> filteredProfiles = __instance.list_0.ApplyFilter(x => (x.Info.Settings.Role == WildSpawnType.assault && !x.WillBeAPlayerScav() || x.Info.Settings.Role != WildSpawnType.assault));
                     __result = data.ChooseProfile(filteredProfiles, withDelete);
 
                     return false;
