1dd8ca6b2812ae9c052171b1f8284e5e20f164ef
ozen-m <74519617+ozen-m@users.noreply.github.com>
2025-04-07 00:31:31 +0800

    Patch BotDespawn method

---
+++ bepinex_dev/SPTQuestingBots/Patches/BotExtractPatch.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using Comfort.Common;
+using UnityEngine;
+using SPT.Reflection.Patching;
+using SPTQuestingBots.Controllers;
+using EFT;
+
+namespace SPTQuestingBots.Patches
+{
+    internal class BotExtractPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(BaseLocalGame<EftGamePlayerOwner>).GetMethod(nameof(BaseLocalGame<EftGamePlayerOwner>.BotDespawn));
+        }
+
+        [PatchPrefix]
+        protected static bool PatchPrefix(BotOwner botOwner)
+        {
+            LoggingController.LogDebug($"{botOwner.GetText()} extracted.");
+
+            var botgame = Singleton<IBotGame>.Instance;
+            botgame.BotsController.BotDied(botOwner);
+            botgame.BotsController.DestroyInfo(botOwner.GetPlayer);
+            Object.DestroyImmediate(botOwner.gameObject);
+            Object.Destroy(botOwner);
+
+            return false;
+        }
+    }
+}
